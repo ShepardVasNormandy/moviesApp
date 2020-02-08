5f286f4691419527426b21fdd9848bfc989d33e9
Enzo de La Rochebrochard <enzo.delarochebrochard@ieva.io>
2020-02-08 16:54:12 +0100

    Modified homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
+import { User } from './../models/user.model';
+import { ApiService } from './../services/api.service';
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    public movies = []
+
+
+    constructor(
+        private navCtrl: NavController,
+        private api: ApiService,
+    ) { }
+
+    ngOnInit() {
+        this.api.getMovieList().then(movies => {
+            for (let key in movies) {
+                this.movies.push(movies[key])
+            }
+            console.log(this.movies)
+        })
+    }
+
 
 }
